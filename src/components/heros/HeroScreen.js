095c1001d41b91f4f47eec3ee23502a1bdf94d11
Your Name <you@example.com>
2021-02-11 22:39:00 -0500

    Se adiciona el componente HeroScreen y valida que exista el hero.

--- src/components/heros/HeroScreen.js
+++ src/components/heros/HeroScreen.js
@@ -1,6 +1,25 @@
 import React from "react";
+import { Redirect, useParams } from "react-router-dom";
+import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
+  const { heroeid } = useParams();
+
+  const hero = getHeroById(heroeid);
+
+  if (!hero) {
+    return <Redirect to="/" />;
+  }
+
+  const {
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+  } = hero;
+
+  console.log(hero);
   return (
     <div>
       <h1>HeroScreen</h1>
