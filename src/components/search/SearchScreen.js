6f3a096812b810b430e2b18110db4ce45c6c22d4
Your Name <you@example.com>
2021-02-12 15:43:28 -0500

    Se adiciona query String.

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
+import queryString from "query-string";
 import { HeroCard } from "../heros/HeroCard";
 import { heroes } from "../../data/data";
 import { getHeroesByName } from "../../selectors/getHeroesByName";
+import { useLocation } from "react-router-dom";
 
-export const SearchScreen = () => {
+export const SearchScreen = ({ history }) => {
+  const location = useLocation();
+  const { q = "" } = queryString.parse(location.search);
   const hero = heroes;
-
-  const [input, setinput] = useState("");
-  const [hero2, sethero2] = useState([]);
+  const [input, setinput] = useState(q);
+  const [hero2, sethero2] = useState(() => getHeroesByName(input));
 
   const handleChange = (event) => {
     setinput(event.target.value);
@@ -15,9 +18,9 @@ export const SearchScreen = () => {
 
   const handleSearch = (event) => {
     event.preventDefault();
-
+    sethero2([]);
     sethero2(getHeroesByName(input));
-    setinput("");
+    history.push(`?q=${input}`);
   };
 
   return (
@@ -50,7 +53,7 @@ export const SearchScreen = () => {
         <div className="col-7">
           <h4> Results </h4>
           <hr />
-          {hero2.length > 0
+          {hero2.length > 0 && q !== ""
             ? hero2.map((hero) => (
                 <div className="mt-2" key={hero.id}>
                   <HeroCard key={hero.id} {...hero} />
